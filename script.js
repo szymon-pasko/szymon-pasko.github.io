c572741d8c7e93cb6c19b42f5a3766c1c3adb062
szymon-pasko <pasko.szymon@gmail.com>
2016-01-20 17:12:54 +0100

    zajecia animacja 20 stycznia

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-$(document).ready(function(){
-    $('login').fadeIn('slow');
-    });
+// $(document).ready(function(){
+//     $('login').fadeIn('slow');
+//     });
+
+$(document).on('click', '.wantstofly',function(){
+	
+	$(this).addClass('bounce');
+	$(this).one('animationend', function (event) {
+		$(this).removeClass('bounce');
+	});
+	
+});
