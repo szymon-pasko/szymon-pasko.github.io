a9b2e57874a35a84e60354a6273bced939595986
szymon-pasko <pasko.szymon@gmail.com>
2016-03-07 17:14:18 +0100

    dalej dalej

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 
 $(document).on('click', '.edit', function () {
     $('.hide').toggleClass('show');
+    $('.edit').toggleClass('hide');
+    $('.save').toggleClass('show'); 
+});
+
+$(document).on('click', '.save', function () {
+    $('.show').toggleClass('hide');
+    $('.edit').toggleClass('show');
+    $('.save').toggleClass('hide'); 
 });
